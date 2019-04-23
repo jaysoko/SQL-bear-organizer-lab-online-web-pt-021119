bc5970c23dceca729aef54b7ecd2aa34392f29e9
Jay Sokolowski <jaysoko@gmail.com>
2019-04-23 02:21:49 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+insert into students (name) values ("Mr. Chocolate");
+insert into students (name) values ("Rowdy");
+insert into students (name) values ("Tabitha");
+insert into students (name) values ("Sergeant Brown");
+insert into students (name) values ("Melissa");
+insert into students (name) values ("Grinch");
+insert into students (name) values ("Wendy");
+
+
+
+
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
