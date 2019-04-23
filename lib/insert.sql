4d9c656dc3f3d7fa8f7c54b8d6a71b6c1492f4e9
Jay Sokolowski <jaysoko@gmail.com>
2019-04-23 02:22:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,17 +1,7 @@
-insert into students (name) values ("Mr. Chocolate");
-insert into students (name) values ("Rowdy");
-insert into students (name) values ("Tabitha");
-insert into students (name) values ("Sergeant Brown");
-insert into students (name) values ("Melissa");
-insert into students (name) values ("Grinch");
-insert into students (name) values ("Wendy");
-
-
-
-
-Rowdy
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
+insert into bears (name) values ("Mr. Chocolate");
+insert into bears (name) values ("Rowdy");
+insert into bears (name) values ("Tabitha");
+insert into bears (name) values ("Sergeant Brown");
+insert into bears (name) values ("Melissa");
+insert into bears (name) values ("Grinch");
+insert into bears (name) values ("Wendy");
