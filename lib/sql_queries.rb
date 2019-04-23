6ca60c011cfd708aee7651a6e869ecfd2e0ee72b
Jay Sokolowski <jaysoko@gmail.com>
2019-04-23 02:35:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "select * from bears where gender="F";"
+  "select * from bears where gender='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select count(temperament) from bears where temperament="goofy";"
 end
 
 def selects_bear_that_killed_Tim
